2c120ca9533a0063f917f72e09bd5e9f12d4e8ec
leandrodias95 <leandrogit95@gmail.com>
2023-12-27 14:53:17 -0300

    validação de dados

--- mercado/src/main/java/com/mercado/rest/ClienteController.java
+++ mercado/src/main/java/com/mercado/rest/ClienteController.java
@@ -1,6 +1,13 @@
 package com.mercado.rest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,8 +36,16 @@ public class ClienteController {
 
 	@PostMapping(value="insert")
 	@ResponseStatus(HttpStatus.CREATED) //resposta da minha requisição
-	public Cliente salvar(@RequestBody @Valid Cliente cliente) {
-		return repository.save(cliente); 
+	public ResponseEntity<Object> salvar(@RequestBody @Valid Cliente cliente, BindingResult result){ //BindingResult extend erros do validator
+		if(result.hasErrors()){
+			Map<String, String> errors = new HashMap<>(); ///associa chave aos valores
+			for(FieldError error: result.getFieldErrors()) {
+				errors.put(error.getField(),error.getDefaultMessage());	
+			}
+			return ResponseEntity.badRequest().body(errors);
+		}
+		Cliente clienteSalvo = repository.save(cliente);
+		return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo);
 	}
 	
 	@GetMapping("{id}")
@@ -50,10 +65,23 @@ public class ClienteController {
 	
 	@PutMapping("{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void atualizar(@PathVariable Long id, @RequestBody @Valid Cliente clienteAtualizar) {
+	public ResponseEntity<Object> atualizar(@PathVariable Long id, @RequestBody @Valid Cliente clienteAtualizar, BindingResult result) {
+		if(!repository.findById(id).isPresent()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		else if(repository.findById(id).isPresent() && result.hasErrors() ) {
+			Map<String, String> errors = new HashMap<>();
+			for(FieldError error: result.getFieldErrors()) {
+				errors.put(error.getField(), error.getDefaultMessage());
+			}
+			return ResponseEntity.badRequest().body(errors);
+		}
 		repository.findById(id).map(cliente->{
 			clienteAtualizar.setId(cliente.getId());
-			return repository.save(clienteAtualizar);
-		}).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+			repository.save(clienteAtualizar);
+			return ResponseEntity.noContent().build();
+		});
+		return ResponseEntity.noContent().build() ; 
 	}
+	
 }
